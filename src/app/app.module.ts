4672d5824b724eb75cdae8db5eb9f05722f177d3
armst0910 <nongarmza@gmail.com>
2016-09-30 15:26:28 +0700

    update angular-cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
 import { TimetableComponent } from './timetable/timetable.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
